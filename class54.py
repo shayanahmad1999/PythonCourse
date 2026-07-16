275d91b8a596dd5f478a667f38ceb13665c2120c
shayanahmad1999 <shayanahmad235@gmail.com>
2026-07-16 15:56:52 +0500

    feat(python): add exercises on modules, standard libraries, and zip functions
    Add new Python exercises (class54.py through class56.py) covering:
    - Importing modules and specific functions from the platform module
    - Utilizing standard libraries including math, random, and datetime
    - Advanced use of zip and unzip operations with iterables and dictionaries

---
+++ class54.py
@@ -0,0 +1,11 @@
+# https://docs.python.org/3/py-modindex.html
+
+# import platform as pl
+
+# print(pl.python_version())
+
+# OR
+
+from platform import python_version as pv
+
+print(pv())
---
+++ class55.py
@@ -0,0 +1,14 @@
+import math as m
+import random as r
+import datetime as dt
+
+print(m.sqrt(25))
+print(m.pi)
+print(m.pow(2,3))
+
+number = r.randint(1, 10)
+print(number)
+
+current_time = dt.datetime.now()
+print(current_time)
+
